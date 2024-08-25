4b06021b122b7f333887fbf9b12147bdfb017379
sofia lappano <152089268+sofi-lapp@users.noreply.github.com>
2024-08-25 12:16:14 -0300

    Libreria

---
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/TpolibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.libreria.tpolibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpolibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpolibreriaApplication.class, args);
+	}
+
+}
