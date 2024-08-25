03fa232b338e0c99d94041867f688595dc0ee0a2
Lola Diaz <137005124+loladiaz1@users.noreply.github.com>
2024-08-25 16:48:46 +0000

    cambio final

---
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/service/CategoryService.java
@@ -0,0 +1,18 @@
+
+package com.uade.tpo.libreria.tpolibreria.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.uade.tpo.libreria.tpolibreria.entity.Category;
+import com.uade.tpo.libreria.tpolibreria.exceptions.CategoryDuplicateException;
+
+public interface CategoryService {
+    public Page<Category> getCategories(PageRequest pageRequest);
+
+    public Optional<Category> getCategoryById(Long categoryId);
+
+    public Category createCategory(String description) throws CategoryDuplicateException;
+}
