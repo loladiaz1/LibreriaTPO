20b6a393ea5bf4df32c0674608deb50f9cbdf02b
sofia lappano <sofiaselenelappano@gmail.com>
2024-08-27 22:46:56 -0300

    relacion con carritos

--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/Carrito.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/Carrito.java
@@ -3,8 +3,12 @@ package com.uade.tpo.libreria.tpolibreria.entity;
 import java.util.HashMap;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
@@ -19,6 +23,20 @@ public class Carrito {
     private double precio;
     @Column
     private HashMap<Libros,Integer> productos; 
+
+    //relacion con usuarios
+    //un usuario tiene un carrito UNICO !!
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuarios usuario;
+
+    //relacion con libros
+    //muchos libros pueden estar en muchos carritos
+    @ManyToMany
+    @ElementCollection
+    private HashMap<Libros, Integer> librosEnCarritos= new HashMap<>();
+    
+
     
 }
 
