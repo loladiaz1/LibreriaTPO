cd2e0269a60effecc0e13c3d4464547e48d7f75a
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2024-09-01 15:41:29 -0300

    getProductosCarrito

--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/controllers/ProductosCarritoController.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/controllers/ProductosCarritoController.java
@@ -2,9 +2,37 @@ package com.uade.tpo.libreria.tpolibreria.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
-@RequestMapping("productos carrito") 
+import com.uade.tpo.libreria.tpolibreria.entity.ProductoCarrito;
+//import com.uade.tpo.libreria.tpolibreria.entity.dto.ProductoCarritoRequest;
+//import com.uade.tpo.libreria.tpolibreria.exceptions.ExcepcionProductoCarritoDuplicado;
+import com.uade.tpo.libreria.tpolibreria.service.ProductoCarritoService;
+
+/* import java.net.URI;
+import java.util.Optional; */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+/* import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody; */
+
+@RestController //indica que esa clase va a manejar solicitudes HTTP y devolverá directamente el resultado de los métodos en forma de respuestas HTTP.
+@RequestMapping("productosCarrito") //indica que todos los métodos de esa clase que manejen solicitudes HTTP tendrán la URL base "/productosCarrito""
 public class ProductosCarritoController {
-    
+    @Autowired
+    private ProductoCarritoService ProductoCarritoService;
+
+    @GetMapping
+    public ResponseEntity<Page<ProductoCarrito>> getProductosCarrito(
+        @RequestParam(required = false) Integer page,
+        @RequestParam(required = false) Integer size) {
+            if (page == null || size == null)
+            return ResponseEntity.ok(ProductoCarritoService.getProductosCarrito(PageRequest.of(0, Integer.MAX_VALUE)));
+        return ResponseEntity.ok(ProductoCarritoService.getProductosCarrito(PageRequest.of(page, size))); 
+    }
+     
 }
