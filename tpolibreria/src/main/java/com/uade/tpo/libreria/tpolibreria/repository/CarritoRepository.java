c3d516dfcb8434e83cc5285d71ba4d30fc2f22ba
sofiaLappano <sofiaselenelappano@gmail.com>
2024-09-01 23:09:47 -0300

    get y post posiblemente terminado

--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/repository/CarritoRepository.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/repository/CarritoRepository.java
@@ -1,18 +1,16 @@
 package com.uade.tpo.libreria.tpolibreria.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.uade.tpo.libreria.tpolibreria.entity.Carrito;
 
 @Repository
 public interface CarritoRepository  extends JpaRepository<Carrito, String>{
 
-    @Query(value = "select c from Carrito c where c.precio = ?1")
-    List<Carrito> findByPrecio(Double precio);
+    @Query(value = "select c from Carrito c where c.nombreUsuario = ?1")
+    List<Carrito> findByNombreUsuario(String nombreUsuario);
 
     
 }
