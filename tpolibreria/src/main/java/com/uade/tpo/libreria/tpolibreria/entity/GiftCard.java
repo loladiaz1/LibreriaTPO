7f6370b6008ca78793573bff79327015c6ac877c
sofia lappano <sofiaselenelappano@gmail.com>
2024-09-03 20:13:33 -0300

    orden y GiftCard sin ejecutar

---
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/GiftCard.java
@@ -0,0 +1,31 @@
+package com.uade.tpo.libreria.tpolibreria.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "gift_cards")
+public class GiftCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //no puede ser null la tabla, se tendria que poner 0 en descuento 
+    //en orden si va a poder ser null
+    @Column(nullable = false)
+    private Double descuento;
+
+     //no puede ser null la tabla y la gift es unica
+    @Column(nullable = false, unique = true)
+    private String codigo;
+
+
+    
+}
--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/Orden.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/Orden.java
@@ -1,5 +1,50 @@
 package com.uade.tpo.libreria.tpolibreria.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "ordenes")
 public class Orden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column
+    private Long count;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "giftCard_id") //permite nulls
+    private GiftCard giftCard;
+
+    @Transient //dato transitorio, no se guarda en la BD
+    private Double precioUnitario;
+
+    @Transient
+    public Double TotalDeCompra() {
+
+        Double total = precioUnitario * count;
+        if (giftCard != null) {
+            // Aplicar descuento 
+            total -= total * (giftCard.getDescuento() / 100);
+        }
+        // Si no hay GiftCard, total ya está sin descuento
+        return total;
+    }
+
     
 }
