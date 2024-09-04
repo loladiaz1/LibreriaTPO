9a1ee388ac80c63e5099caf3c8018bca7ef27bfd
sofia lappano <sofiaselenelappano@gmail.com>
2024-09-03 22:46:33 -0300

    orden sin get y post

--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/Orden.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/entity/Orden.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.Data;
@@ -21,15 +22,12 @@ public class Orden {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column
-    private Long count;
 
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "giftCard_id") //permite nulls
     private GiftCard giftCard;
 
@@ -38,18 +36,14 @@ public class Orden {
     @JsonBackReference
     private Carrito carrito;
 
-    @Transient //dato transitorio, no se guarda en la BD
-    private Double precioUnitario;
-
-    @Transient
+    @Transient//dato transitorio, no se guarda en la BD
     public Double TotalDeCompra() {
-
-        Double total = precioUnitario * count;
+        Double total = carrito.getPrecio();
         if (giftCard != null) {
             // Aplicar descuento 
             total -= total * (giftCard.getDescuento() / 100);
         }
-        // Si no hay GiftCard, total ya está sin descuento
+        // Si no hay GiftCard, total ya está sin el descuento
         return total;
     }
 
