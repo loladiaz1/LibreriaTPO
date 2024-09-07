0a9d54b3c8cfe7ce097cdc1ed997dd0c8076bbe9
sofiaLappano <csalemme@uade.edu.ar>
2024-09-06 23:49:54 -0300

    Seguridad mas especfica

--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/controllers/config/SecurityConfig.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/controllers/config/SecurityConfig.java
@@ -11,9 +11,15 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.uade.tpo.libreria.tpolibreria.entity.Carrito;
+import com.uade.tpo.libreria.tpolibreria.entity.ProductoCarrito;
 import com.uade.tpo.libreria.tpolibreria.entity.Role;
+import com.uade.tpo.libreria.tpolibreria.entity.Usuario;
 import com.uade.tpo.libreria.tpolibreria.repository.CarritoRepository;
+import com.uade.tpo.libreria.tpolibreria.repository.ProductoCarritoRepository;
+import com.uade.tpo.libreria.tpolibreria.repository.UsuarioRepository;
 import com.uade.tpo.libreria.tpolibreria.service.CarritoService;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -27,27 +33,46 @@ public class SecurityConfig {
 
         private final JwtAuthenticationFilter jwtAuthFilter;
         private final AuthenticationProvider authenticationProvider;
-        private CarritoRepository carritoRepository;
+        private final CarritoRepository carritoRepository;
+        private final ProductoCarritoRepository productoCarritoRepository;
+        private final UsuarioRepository usuarioRepository;
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
                                 .csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/**").permitAll()
-                                                .requestMatchers("/error/**").permitAll()
-                                                .requestMatchers("/generos/**").permitAll()
-                                                .requestMatchers("/libros/**").permitAll()
-                                                .requestMatchers("/carritos/**").hasAnyAuthority(Role.USUARIO.name())
-                                                .requestMatchers("/productosCarrito/**").hasAnyAuthority(Role.USUARIO.name())
-                                                .requestMatchers("/usuarios/**").hasAnyAuthority(Role.ADMIN.name())
-                                                //.requestMatchers("/carritos/{carritoId}").access((authentication, context) -> {
-                                                //        List<Carrito> carrito = carritoRepository.findByMail(authentication.get().getName());
-                                                //        return new AuthorizationDecision(carrito.isEmpty()); })
-                                                //.requestMatchers("/carritos/{carritoId}").hasAnyAuthority(Role.USUARIO.name())
-                                                //.requestMatchers("/productosCarrito/{productoCarritoId}").hasAnyAuthority(Role.USUARIO.name())
-                                                .requestMatchers("/usuarios/{UsuarioId}").hasAnyAuthority(Role.USUARIO.name())
-                                                .anyRequest()
-                                                .authenticated())
+                                        .requestMatchers("/error/**").permitAll()
+                                        .requestMatchers("/carritos/{carritoId}").access((authentication, context) -> {
+                                                String carritoId = context.getVariables().get("carritoId");
+                                                Optional<Carrito> carrito = carritoRepository.findById(carritoId);
+                                                boolean esPropietario = (carrito.isPresent() && carrito.get().getMail().equals(authentication.get().getName()));
+                                                boolean esAdmin = authentication.get().getAuthorities().stream()
+                                                                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.ADMIN.name()));
+                                                return new AuthorizationDecision(esPropietario || esAdmin); })
+                                       /*  .requestMatchers("/productosCarrito/{productoCarritoId}").access((authentication, context) -> {
+                                                String productoCarritoId = context.getVariables().get("productoCarritoId");
+                                                Optional<ProductoCarrito> productoCarrito = productoCarritoRepository.findByMail(productoCarritoId);
+                                                boolean esPropietario = (carrito.isPresent() && carrito.get().getMail().equals(authentication.get().getName()));
+                                                boolean esAdmin = authentication.get().getAuthorities().stream()
+                                                                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.ADMIN.name()));
+                                                return new AuthorizationDecision(esPropietario || esAdmin); })
+                                                Hacer prodcuto carrito by mail*/
+                                        /* .requestMatchers("/usuarios/{UsuarioId}").access((authentication, context) -> {
+                                                Long usuarioId = Long.parseLong(context.getVariables().get("UsuarioId"));
+                                                Optional<Usuario> usuario = usuarioRepository.findById(usuarioId);
+                                                boolean esPropietario = (usuario.isPresent() && usuario.get().getId().equals(authentication.get().getName()));
+                                                boolean esAdmin = authentication.get().getAuthorities().stream()
+                                                                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.ADMIN.name()));
+                                                return new AuthorizationDecision(esPropietario || esAdmin); })
+                                                hacer usuario by mail*/
+                                        .requestMatchers("/carritos/**").hasAnyAuthority(Role.ADMIN.name())
+                                        .requestMatchers("/productosCarrito/**").hasAnyAuthority(Role.ADMIN.name())
+                                        .requestMatchers("/usuarios/**").hasAnyAuthority(Role.ADMIN.name())
+                                        .requestMatchers("/generos/**").permitAll()
+                                        .requestMatchers("/libros/**").permitAll()
+                                        .anyRequest()
+                                        .authenticated())
                                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                                 .authenticationProvider(authenticationProvider)
                                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/repository/ProductoCarritoRepository.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/repository/ProductoCarritoRepository.java
@@ -1,5 +1,6 @@
 package com.uade.tpo.libreria.tpolibreria.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 //import java.util.List;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.uade.tpo.libreria.tpolibreria.entity.Carrito;
 import com.uade.tpo.libreria.tpolibreria.entity.ProductoCarrito;
 
 @Repository
@@ -18,4 +20,7 @@ public interface ProductoCarritoRepository extends JpaRepository<ProductoCarrito
     
     @Query("SELECT p.cantidad FROM ProductoCarrito p WHERE p.id = :id")
     Optional<Integer> findCantidadById(@Param("id") Long id);
+
+    @Query(value = "select p from ProductoCarrito p where p.carrito_mail = ?1")
+    List<Carrito> findByMail(String mail);
 } 
