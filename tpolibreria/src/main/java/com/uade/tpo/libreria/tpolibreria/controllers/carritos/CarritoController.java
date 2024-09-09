122939d641f9feef184b67f07d51db3e26344ab9
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2024-09-09 10:26:45 -0300

    eliminacion de imports

--- tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/controllers/carritos/CarritoController.java
+++ tpolibreria/src/main/java/com/uade/tpo/libreria/tpolibreria/controllers/carritos/CarritoController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.uade.tpo.libreria.tpolibreria.controllers.productosCarrito.ProductoCarritoRequest;
 import com.uade.tpo.libreria.tpolibreria.entity.Carrito;
 import com.uade.tpo.libreria.tpolibreria.exceptions.ExcepcionCarrito;
 import com.uade.tpo.libreria.tpolibreria.service.CarritoService;
